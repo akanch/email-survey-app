6597763f8b8a05e3c6a2562b561d87214e2f1b8c
Allen Kan <akanch95@gmail.com>
2018-12-07 09:22:42 -0800

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // create new route handler
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hello: 'world' });
 });
 
 const PORT = process.env.PORT || 5000;
