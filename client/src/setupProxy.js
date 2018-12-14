c7739be2d20bc9cf61e4c4637ede3fd0c7cc300c
Allen Kan <akanch95@gmail.com>
2018-12-14 14:05:11 -0800

    added notes for proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,3 +1,10 @@
+// proxy only used in dev since we need both react and express servers
+// in prod react server gets bundled by webpack and babel and goes away
+// an additional benefit of a proxy is that when a broswer makes an AJAX
+// request from localhost:3000 to localhost:5000, cookies(the primary
+// piece of ID), will not be included for security purposes. Since the
+// browser is unaware of the proxy, cookies can be passed from
+// localhost:3000(react server) to localhost:5000(express server)
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
