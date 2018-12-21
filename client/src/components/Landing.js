464e5bf4d1844d14b363c159f0df5daebe12461a
Allen Kan <akanch95@gmail.com>
2018-12-21 14:08:41 -0800

    updated landing page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>
+        Wudi!
+      </h1>
+      Collect feedback from your customers.
+    </div>
+  );
+};
+
+export default Landing;
