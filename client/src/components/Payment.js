30662a95b69754cabfea38a7052197e035a336b7
Allen Kan <akanch95@gmail.com>
2018-12-21 18:47:19 -0800

    add credit button added to header

---
+++ client/src/components/Payment.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+class Payment extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Wudi"
+        description="$5 for 5 email credits"
+        // amount is measured in cents, the amount is how much we are requesting
+        // from the user
+        amount={500}
+        // token expects to receive a callback function, and it s called after
+        // we receive the authorization token from Stripe's api
+        token={token => console.log(token)}
+        // the publishable api key
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payment;
