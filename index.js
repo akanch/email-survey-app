554c879b6d29883f1dd593f76d2aa085e2998e2b
Allen Kan <akanch95@gmail.com>
2018-12-27 15:51:17 -0800

    fixed error

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
   // checking in client/build
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", " build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   })
 }
 
