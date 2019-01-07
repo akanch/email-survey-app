93aae7af4db4f54230109d78e408fdd86b766458
Allen Kan <akanch95@gmail.com>
2019-01-07 14:11:47 -0800

    ensure that cancel button prevents user input from persisting

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,6 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
 
@@ -29,8 +30,10 @@ class SurveyNew extends Component {
       <div>
         {this.renderContent()}
       </div>
-    )
+    );
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyNew);
